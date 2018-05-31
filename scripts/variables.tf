4b4626c436e1aed2a07908f714b289cf99618ae7
Jagadt <jagadt@hotmail.com>
2018-05-31 14:03:45 +0530

    Create variables.tf

---
+++ scripts/variables.tf
@@ -0,0 +1,41 @@
+# Azure Provider Variables
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+variable "location" {
+    default = "Southeast Asia"
+}
+
+# Instance Variables
+variable "tags" {
+    default = {
+        environment = "staging"
+        created_by = "terraform"
+    }
+}
+
+variable "prefix" {
+    default = "Drupal"
+}
+
+variable "servers" {
+    default = 1
+    description = "The number of App servers to launch."
+}
+
+variable "vm_size" {
+    default = "Standard_A0"
+    description = "Size of the VM. See https://azure.microsoft.com/en-us/documentation/articles/virtual-machines-windows-sizes/"
+}
+
+variable "user_login" {
+    default = "testuser"
+    description = "Default login for this image"
+}
+
+variable "user_password" {
+    default = "Password1234!"
+    description = "Default password for this image"
+}
+
